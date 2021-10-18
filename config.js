3ed9bf07bac146732b51314711070cd239802faa
trevismurithi <trevismurithi@gmail.com>
2021-10-18 09:54:47 +0300

    remove key and secret

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     'linkedinAuth': {
-        'clientID': '77ov3qq46zcg4z', // your App ID
-        'clientSecret': '7Gl4UGhmAK75X8zR', // your App Secret
+        'clientID': '', // your App ID
+        'clientSecret': '', // your App Secret
         'callbackURL': 'http://localhost:3000/auth/linkedin/callback'
     }
 }
