70a35386a3f75e08edd7cdbc774d64c0fcbe1ad1
trevismurithi <trevismurithi@gmail.com>
2021-10-18 09:50:39 +0300

    git init

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'linkedinAuth': {
+        'clientID': '77ov3qq46zcg4z', // your App ID
+        'clientSecret': '7Gl4UGhmAK75X8zR', // your App Secret
+        'callbackURL': 'http://localhost:3000/auth/linkedin/callback'
+    }
+}
